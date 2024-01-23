f20fa7da86f6dacf80df7b5c9d4a926658b3baa1
eternalmoon1234 <gautamparanjape8@gmail.com>
2024-01-22 22:34:44 -0800

    added vendordeps + linter

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2024-Beta";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "325ebb780479b5f6696d5de4ebb9eef113a31124";
+  public static final String GIT_DATE = "2024-01-23 01:25:13 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-01-23 01:34:19 EST";
+  public static final long BUILD_UNIX_TIME = 1705991659494L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
