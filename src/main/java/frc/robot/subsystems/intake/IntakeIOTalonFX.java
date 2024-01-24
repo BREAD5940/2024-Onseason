0a2c202241919b7af8c153d2497684fb34bfa238
JamesEHart <jhart27@dtechhs.org>
2024-01-23 17:37:29 -0800

    made the intakeIOtalonfx
    made new file

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems.intake;
+
+public class IntakeIOTalonFX implements IntakeIO {
+  // create motor
+
+  public IntakeIOTalonFX() {
+    // create configuration
+
+    // supply current limit
+
+    // motor direction
+
+    // set motor to brake mode
+
+    // apply configuration to motor
+  }
+
+  @Override
+  public void updateInputs(IntakeIOInputs inputs) {
+    // update positionRad, velocityRpm, appliedVolts, and currentAmps
+  } 
+  
+  @Override
+  public void setPercent(double percent) {
+
+  }
+}
