815eec076a54e002f8a4553070a59eca8e8ed7f7
JamesEHart <jhart27@dtechhs.org>
2024-01-28 17:48:32 -0800

    fixed errors

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -5,12 +5,17 @@ import edu.wpi.first.math.geometry.Rotation2d;
 public class Constants {
 
   public static final boolean tuningMode = true;
+  public static final String CANBUS_NAME = "dabus";
 
   /* Coast pertaining to the swerve drive */
   public static class Swerve {
 
     public static double COAST_TRESHOLD_MPS = 0.05;
     public static double COAST_TRESHOLD_SEC = 5.0;
+  }
+
+  public static class Intake {
+    public static int INTAKE_ID = 59;
   }
 
   /* Constants pertaining to the elevator */
