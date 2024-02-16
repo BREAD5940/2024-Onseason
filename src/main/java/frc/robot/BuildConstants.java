d54f2cf117feb547348ed73971b2043762277bcc
Krish Kittur <73321517+KrishKittur@users.noreply.github.com>
2024-02-16 13:35:31 -0800

    Clean up photon implementation

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-Onseason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 30;
-  public static final String GIT_SHA = "d7fd816c1a3fb3f1a11700dfa4b6858d90806d04";
-  public static final String GIT_DATE = "2024-02-15 18:50:03 EST";
-  public static final String GIT_BRANCH = "beta-dev";
-  public static final String BUILD_DATE = "2024-02-15 20:23:33 EST";
-  public static final long BUILD_UNIX_TIME = 1708046613334L;
+  public static final int GIT_REVISION = 31;
+  public static final String GIT_SHA = "55994865bd71b655254594659c9a0d24a69ea39e";
+  public static final String GIT_DATE = "2024-02-15 20:32:03 EST";
+  public static final String GIT_BRANCH = "photonvision-apriltag-dev";
+  public static final String BUILD_DATE = "2024-02-16 16:31:31 EST";
+  public static final long BUILD_UNIX_TIME = 1708119091716L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/vision/photonvision/PhotonAprilTagVision.java
+++ src/main/java/frc/robot/vision/photonvision/PhotonAprilTagVision.java
@@ -1,5 +1,7 @@
 package frc.robot.vision.photonvision;
 
+import static frc.robot.constants.FieldConstants.aprilTags;
+
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
@@ -7,6 +9,8 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.commons.GeomUtil;
+import frc.robot.commons.LoggedTunableNumber;
+import frc.robot.commons.PolynomialRegression;
 import frc.robot.commons.TimestampedVisionUpdate;
 import frc.robot.constants.FieldConstants;
 import java.util.ArrayList;
@@ -15,21 +19,36 @@ import java.util.function.Consumer;
 import java.util.function.Supplier;
 import org.littletonrobotics.junction.Logger;
 import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class PhotonAprilTagVision extends SubsystemBase {
   private PhotonCamera[] cameras;
-  private PhotonPoseEstimator[] photonEstimators;
   private static final double fieldBorderMargin = 0.5;
   private double xyStdDevCoefficient = 0.006;
   private double thetaStdDevCoefficient = 0.002;
   private Consumer<List<TimestampedVisionUpdate>> visionConsumer = x -> {};
   private List<TimestampedVisionUpdate> visionUpdates;
-  private List<Pose2d> allRobotPoses;
   private Supplier<Pose2d> poseSupplier = () -> new Pose2d();
-  private Pose2d currentPose;
+
+  public static LoggedTunableNumber mStdDevScalar =
+      new LoggedTunableNumber("AprilTagVision/StdDevScalar", 2.0);
+  private PolynomialRegression xyStdDevModel =
+      new PolynomialRegression(
+          new double[] {
+            0.752358, 1.016358, 1.296358, 1.574358, 1.913358, 2.184358, 2.493358, 2.758358,
+            3.223358, 4.093358, 4.726358
+          },
+          new double[] {0.005, 0.0135, 0.016, 0.038, 0.0515, 0.0925, 0.12, 0.14, 0.17, 0.27, 0.38},
+          2);
+  private PolynomialRegression thetaStdDevModel =
+      new PolynomialRegression(
+          new double[] {
+            0.752358, 1.016358, 1.296358, 1.574358, 1.913358, 2.184358, 2.493358, 2.758358,
+            3.223358, 4.093358, 4.726358
+          },
+          new double[] {0.008, 0.027, 0.015, 0.044, 0.04, 0.078, 0.049, 0.027, 0.059, 0.029, 0.068},
+          1);
 
   private final Pose3d[] cameraPoses =
       new Pose3d[] {
@@ -73,16 +92,6 @@ public class PhotonAprilTagVision extends SubsystemBase {
 
   public PhotonAprilTagVision(PhotonCamera... cameras) {
     this.cameras = cameras;
-    photonEstimators = new PhotonPoseEstimator[cameras.length];
-    for (int i = 0; i < cameras.length; i++) {
-      photonEstimators[i] =
-          new PhotonPoseEstimator(
-              FieldConstants.aprilTags,
-              PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-              cameras[i],
-              GeomUtil.pose3dToTransform3d(cameraPoses[i]));
-      photonEstimators[i].setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
-    }
   }
 
   public void setDataInterfaces(
@@ -93,59 +102,86 @@ public class PhotonAprilTagVision extends SubsystemBase {
 
   @Override
   public void periodic() {
-    Logger.recordOutput("Cameras Length", cameras.length);
 
+    Pose2d currentPose = poseSupplier.get();
+    visionUpdates = new ArrayList<>();
+
+    // Loop through all the cameras
     for (int instanceIndex = 0; instanceIndex < cameras.length; instanceIndex++) {
-      allRobotPoses = new ArrayList<>();
-      visionUpdates = new ArrayList<>();
-      currentPose = poseSupplier.get();
 
+      // Camera-specific variables
+      Pose3d cameraPose;
+      Pose2d robotPose;
       List<Pose3d> tagPose3ds = new ArrayList<>();
-      Pose2d robotPose = null;
-      Pose3d cameraPose = null;
-      double timestamp = 0.0;
-      var latestCameraResult = cameras[instanceIndex].getLatestResult();
+      PhotonPipelineResult latestCameraResult = cameras[instanceIndex].getLatestResult();
 
       Logger.recordOutput(
-          "Camera " + instanceIndex + " Has Targets", latestCameraResult.hasTargets());
+          "Photon/Camera " + instanceIndex + " Has Targets", latestCameraResult.hasTargets());
 
-      if (latestCameraResult.hasTargets()) {
-        photonEstimators[instanceIndex].setReferencePose(currentPose);
-        var latestVisionEstimation = photonEstimators[instanceIndex].update();
-
-        // robotPose = latestVisionEstimation.get().estimatedPose.toPose2d();
-        // timestamp = latestVisionEstimation.get().timestampSeconds;
-        robotPose = new Pose2d();
-        timestamp = 0.0;
-
-        System.out.println(photonEstimators[instanceIndex].getRobotToCameraTransform());
-
-        if (latestCameraResult.getMultiTagResult().estimatedPose.isPresent) {
-          cameraPose =
-              GeomUtil.transform3dToPose3d(
-                  latestCameraResult.getMultiTagResult().estimatedPose.best);
-        } else {
-          cameraPose =
-              GeomUtil.transform3dToPose3d(
-                  cameras[instanceIndex].getLatestResult().getBestTarget().getBestCameraToTarget());
-        }
-
-        for (PhotonTrackedTarget trackedTarget : latestVisionEstimation.get().targetsUsed) {
-          int fiducialId = trackedTarget.getFiducialId();
-          Pose3d tagPose = FieldConstants.aprilTags.getTagPose(fiducialId).get();
-
-          if (tagPose != null) {
-            tagPose3ds.add(tagPose);
-          }
-        }
-      }
-
-      // Exit if no data
-      if (robotPose == null || cameraPose == null) {
+      // Continue if the camera doesn't have any targets
+      if (!latestCameraResult.hasTargets()) {
         continue;
       }
 
-      // Exit if robot pose is off the field
+      double timestamp = latestCameraResult.getTimestampSeconds();
+      Logger.recordOutput("Photon/Camera " + instanceIndex + " Timestamp", timestamp);
+
+      boolean shouldUseMultiTag = latestCameraResult.getMultiTagResult().estimatedPose.isPresent;
+
+      if (shouldUseMultiTag) {
+        // If multitag, use directly
+        cameraPose = GeomUtil.transform3dToPose3d(latestCameraResult.getMultiTagResult().estimatedPose.best);
+
+        robotPose = cameraPose
+            .transformBy(GeomUtil.pose3dToTransform3d(cameraPoses[instanceIndex]).inverse())
+            .toPose2d();
+
+        // Populate array of tag poses with tags used
+        for (int id : latestCameraResult.getMultiTagResult().fiducialIDsUsed) {
+          tagPose3ds.add(aprilTags.getTagPose(id).get());
+        }
+
+        Logger.recordOutput("Photon/Camera Pose (Multi tag) " + instanceIndex, cameraPose);
+      } else {
+        // If not using multitag, disambiugate and then use
+        PhotonTrackedTarget target = latestCameraResult.targets.get(0);
+
+        Pose3d tagPos = aprilTags.getTagPose(target.getFiducialId()).get();
+
+        Pose3d cameraPose0 = tagPos.transformBy(target.getBestCameraToTarget().inverse());
+        Pose3d cameraPose1 = tagPos.transformBy(target.getAlternateCameraToTarget().inverse());
+        Pose2d robotPose0 = cameraPose0
+            .transformBy(GeomUtil.pose3dToTransform3d(cameraPoses[instanceIndex]).inverse())
+            .toPose2d();
+        Pose2d robotPose1 = cameraPose1
+            .transformBy(GeomUtil.pose3dToTransform3d(cameraPoses[instanceIndex]).inverse())
+            .toPose2d();
+
+        double projectionError = target.getPoseAmbiguity();
+
+        // Select a pose using projection error and current rotation
+        if (projectionError < 0.15) {
+          cameraPose = cameraPose0;
+          robotPose = robotPose0;
+        } else if (Math.abs(robotPose0.getRotation().minus(currentPose.getRotation()).getRadians()) < Math
+            .abs(robotPose1.getRotation().minus(currentPose.getRotation()).getRadians())) {
+          cameraPose = cameraPose0;
+          robotPose = robotPose0;
+        } else {
+          cameraPose = cameraPose1;
+          robotPose = robotPose1;
+        }
+
+        tagPose3ds.add(tagPos);
+        Logger.recordOutput("Photon/Camera Pose (Single Tag) " + instanceIndex, cameraPose);
+      }
+
+      // TODO not sure if we need this but I'll just leave it here
+      if (cameraPose == null || robotPose == null) {
+        continue;
+      }
+
+      // Move on to next camera if robot pose is off the field
       if (robotPose.getX() < -fieldBorderMargin
           || robotPose.getX() > FieldConstants.fieldLength + fieldBorderMargin
           || robotPose.getY() < -fieldBorderMargin
@@ -162,16 +198,22 @@ public class PhotonAprilTagVision extends SubsystemBase {
       double xyStdDev = 0.0;
       double thetaStdDev = 0.0;
 
-      xyStdDev = xyStdDevCoefficient * Math.pow(avgDistance, 2.0) / tagPose3ds.size();
-      thetaStdDev = thetaStdDevCoefficient * Math.pow(avgDistance, 2.0) / tagPose3ds.size();
+      if (shouldUseMultiTag) {
+        xyStdDev = xyStdDevCoefficient * Math.pow(avgDistance, 2.0) / tagPose3ds.size();
+        thetaStdDev = thetaStdDevCoefficient * Math.pow(avgDistance, 2.0) / tagPose3ds.size();
+      } else {
+        xyStdDev = xyStdDevModel.predict(avgDistance) * mStdDevScalar.get();
+        thetaStdDev = thetaStdDevModel.predict(avgDistance) * mStdDevScalar.get();
+      }
 
       visionUpdates.add(
           new TimestampedVisionUpdate(
               robotPose, timestamp, VecBuilder.fill(xyStdDev, xyStdDev, thetaStdDev)));
-      allRobotPoses.add(robotPose);
 
-      Logger.recordOutput("PhotonTagVision/VisionUpdates", visionUpdates.size());
-      Logger.recordOutput("PhotonTagVision/VisionData" + instanceIndex, robotPose);
+      Logger.recordOutput("VisionData/" + instanceIndex, robotPose);
     }
+    
+    // Apply all vision updates to pose estimator
+    visionConsumer.accept(visionUpdates);
   }
 }
