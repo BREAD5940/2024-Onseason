9aa54e10ec32d885ba2ef74a110c463adefc7c5e
Krish Kittur <73321517+KrishKittur@users.noreply.github.com>
2024-04-07 23:04:33 -0700

    Post EBR Commit

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-Onseason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 82;
-  public static final String GIT_SHA = "eeb2655d8667ffa83464f52c2fe1913b303d4451";
-  public static final String GIT_DATE = "2024-04-06 11:02:37 EDT";
+  public static final int GIT_REVISION = 83;
+  public static final String GIT_SHA = "e21354187f174003d1252884ac280b4b24d8aa51";
+  public static final String GIT_DATE = "2024-04-06 22:05:27 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-04-06 13:41:04 EDT";
-  public static final long BUILD_UNIX_TIME = 1712425264190L;
+  public static final String BUILD_DATE = "2024-04-07 16:32:42 EDT";
+  public static final long BUILD_UNIX_TIME = 1712521962404L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
