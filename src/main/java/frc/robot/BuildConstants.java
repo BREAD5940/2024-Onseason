21eebfe7c7ebfa01f5afea1490204b8c0a4668e1
Theo Wilden <143763556+DtechTwilden@users.noreply.github.com>
2024-08-25 17:03:11 -0700

    Build fix (Hopefully)
    Photonvision repository url error fix and added Jama-1.0.3.jar

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-Onseason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 94;
-  public static final String GIT_SHA = "d62a8fc1d729caa0e79e377b439fbbbe2290a49f";
-  public static final String GIT_DATE = "2024-05-19 21:55:08 EDT";
+  public static final int GIT_REVISION = 95;
+  public static final String GIT_SHA = "3576530691a673804011a1b2c1fcbb1b1606075b";
+  public static final String GIT_DATE = "2024-07-19 13:30:07 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-07-18 22:38:33 EDT";
-  public static final long BUILD_UNIX_TIME = 1721356713627L;
+  public static final String BUILD_DATE = "2024-08-25 19:56:43 EDT";
+  public static final long BUILD_UNIX_TIME = 1724630203232L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
